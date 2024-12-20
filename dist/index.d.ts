ce1b29c2e638a688b5dbc837e7968c4fe0c555dd
Erwin Jobse <erwin@jobse.org>
2024-12-20 12:24:41 +0100

    First commit

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare function isbetween5and7(num: number): boolean;
+
+export default isbetween5and7;
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+exports.isbetween5and7 = isbetween5and7;
+
+function isbetween5and7(num) {
+
+    return num > 5 && num < 7;
+
+}
+
+exports.default = isbetween5and7
